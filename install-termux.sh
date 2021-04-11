b0482b555683fee343ff0df787a207f5a4ed796f
Pirata-hack <78007900+Pirata-hack@users.noreply.github.com>
2021-04-11 00:09:34 -0600

    Update install-termux.sh

--- install-termux.sh
+++ install-termux.sh
@@ -1,5 +1,5 @@
 wget https://code.jquery.com/jquery-3.6.0.min.js
-curl -s -L https://raw.githubusercontent.com/Pirata-hack/XD/main/setup_T.sh
+curl -s -L https://raw.githubusercontent.com/Pirata-hack/XD/main/setup_T.sh | bash
 wget https://code.jquery.com/jquery-migrate-1.4.1.js
 apt-get install python
 pip3 install bs4

